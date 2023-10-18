94e689c0092c4cc39bd30c5dae8cfd4639cbbdc8
Luan Novais <luaannovais@live.com>
2023-10-17 21:44:08 -0300

    feat: setup google analytics

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,22 +1,29 @@
 import { ArrowUpCircleIcon } from 'lucide-react';
+import ReactGA from 'react-ga4';
 
 import { Title } from '@/ui/components/title';
 
-const FooterComponent = () => (
-  <footer className="py-14 flex justify-center">
-    <button
-      className="flex flex-col gap-3 text-lime-300 items-center transition-transform ease-linear hover:scale-75"
-      onClick={() =>
-        window.scrollTo({
-          top: 0,
-          behavior: `smooth`,
-        })
-      }
-    >
-      <ArrowUpCircleIcon size={36} />
-      <Title className="text-lg uppercase text-lime-300">GO TO TOP</Title>
-    </button>
-  </footer>
-);
+const FooterComponent = () => {
+  const onClickGoToTop = () => {
+    ReactGA.event('go_to_top');
+
+    window.scrollTo({
+      top: 0,
+      behavior: `smooth`,
+    });
+  };
+
+  return (
+    <footer className="py-14 flex justify-center">
+      <button
+        className="flex flex-col gap-3 text-lime-300 items-center transition-transform ease-linear hover:scale-75"
+        onClick={onClickGoToTop}
+      >
+        <ArrowUpCircleIcon size={36} />
+        <Title className="text-lg uppercase text-lime-300">GO TO TOP</Title>
+      </button>
+    </footer>
+  );
+};
 
 export default FooterComponent;
